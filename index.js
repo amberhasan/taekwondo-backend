37bda158dcf4160a85bc852cd5f4e1f3c1e6717b
amberhasan <amber.hasan@gmail.com>
2023-08-05 11:53:10 -0500

    GET and simple POST logging

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require("express");
 // import express from 'express';
 const app = express();
+app.use(express.json()); //this tells the app to use JSON to pass the body during requests like POST
 const port = 3000;
+
+const users = [
+  {
+    id: 1,
+    name: "Safdar",
+    lastName: "Sikander",
+  },
+];
 
 // http://127.0.0.1:3000/
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/amber", (req, res) => {
-  res.send("Hello Amber!");
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  console.log("req.body", req.body);
 });
 
 // get
