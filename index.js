d192ba087a6585bef8e66a06ad08bf6cdd0c85c9
amberhasan <amber.hasan@gmail.com>
2023-08-08 17:17:09 -0500

    Pulled the belt colors into the page

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { menu } = require("./mock-data");
+const { menu, belt_test_requirements, belt_colors } = require("./mock-data");
 // import express from 'express';
 const app = express();
 app.use(express.json()); //this tells the app to use JSON to pass the body during requests like POST
@@ -7,6 +7,14 @@ const port = 3000;
 
 app.get("/menu", (request, response) => {
   response.json(menu);
+});
+
+// app.get("/belts/:id", (request, response) => {
+//   response.json(belt_test_requirements);
+// });
+
+app.get("/belts", (request, response) => {
+  response.json(belt_colors);
 });
 
 app.listen(port, () => {
--- mock-data.js
+++ mock-data.js
@@ -1,52 +1,247 @@
 const menu = [
   {
+    id: 1,
     title: "Our Special",
     description: "Try our $99 5-week summer special",
     image: "https://leesustaekwondo.com/assets/img/homepage/hp13.webp",
   },
   {
+    id: 2,
     title: "Children's Classes",
     description:
       "Classes for children 10 and under who want to learn Taekwondo",
     image: "https://leesustaekwondo.com/assets/img/homepage/hp13.webp",
   },
   {
+    id: 3,
+
     title: "Adult/Teen Classes",
     description:
       "Classes for teens and adults 11-65+ who want to learn Taekwondo",
     image: "https://leesustaekwondo.com/assets/img/homepage/hp13.webp",
   },
   {
+    id: 4,
+
     title: "Demo Team",
     description: "Our Taekwondo school's demonstration team",
     image: "https://leesustaekwondo.com/assets/img/homepage/hp13.webp",
   },
   {
+    id: 5,
+
     title: "Poomsae Team",
     description:
       "For students with the desire to compete at a professional level",
     image: "https://leesustaekwondo.com/assets/img/homepage/hp13.webp",
   },
   {
+    id: 6,
+
     title: "Our School Championship",
     description: "Our Taekwondo school's yearly competition",
     image: "https://leesustaekwondo.com/assets/img/homepage/hp13.webp",
   },
   {
+    id: 7,
+
     title: "Summer Reading Program",
     description: "Learn about the program and download the reading log",
     image: "https://leesustaekwondo.com/assets/img/homepage/hp13.webp",
   },
   {
+    id: 8,
+
     title: "Birthday Parties",
     description: "Learn more about having a birthday party with Taekwondo",
     image: "https://leesustaekwondo.com/assets/img/homepage/hp13.webp",
   },
   {
+    id: 9,
+
     title: "Belt Requirements",
     description: "For current students",
     image: "https://leesustaekwondo.com/assets/img/homepage/hp13.webp",
   },
 ];
 
-module.exports = { menu };
+const belt_test_requirements = [
+  {
+    belt_test_requirements: {
+      junior: {
+        white: {
+          palgwae: [],
+          taeguek: [],
+          white_belt_form: [1, 2],
+          combination: [1, 2, 3, 4, 5],
+          step_match: [1, 2],
+          kick: ["front kick", "turn kick"],
+          sparring: "1:1",
+          breaking: ["palm", "front kick"],
+        },
+        white_yellow_stripe: {
+          palgwae: [1],
+          taeguek: [],
+          white_belt_form: [1, 2],
+          combination: [1, 2, 3, 4, 5],
+          step_match: [1, 2],
+          kick: ["front kick", "turn kick"],
+          sparring: "1:1",
+          breaking: ["palm", "front kick"],
+        },
+        yellow: {
+          palgwae: [],
+          taeguek: [],
+          white_belt_form: [1, 2],
+          combination: [1, 2, 3, 4, 5],
+          step_match: [1, 2],
+          kick: ["front kick", "turn kick"],
+          sparring: "1:1",
+          breaking: ["palm", "front kick"],
+        },
+        green: {
+          palgwae: [],
+          taeguek: [],
+          white_belt_form: [1, 2],
+          combination: [1, 2, 3, 4, 5],
+          step_match: [1, 2],
+          kick: ["front kick", "turn kick"],
+          sparring: "1:1",
+          breaking: ["palm", "front kick"],
+        },
+        blue: {
+          palgwae: [],
+          taeguek: [],
+          white_belt_form: [1, 2],
+          combination: [1, 2, 3, 4, 5],
+          step_match: [1, 2],
+          kick: ["front kick", "turn kick"],
+          sparring: "1:1",
+          breaking: ["palm", "front kick"],
+        },
+        red: {
+          palgwae: [],
+          taeguek: [],
+          white_belt_form: [1, 2],
+          combination: [1, 2, 3, 4, 5],
+          step_match: [1, 2],
+          kick: ["front kick", "turn kick"],
+          sparring: "1:1",
+          breaking: ["palm", "front kick"],
+        },
+        first_dan_evaluation: {
+          palgwae: [],
+          taeguek: [],
+          white_belt_form: [1, 2],
+          combination: [1, 2, 3, 4, 5],
+          step_match: [1, 2],
+          kick: ["front kick", "turn kick"],
+          sparring: "1:1",
+          breaking: ["palm", "front kick"],
+        },
+      },
+      senior: {
+        white: {
+          palgwae: [],
+          taeguek: [],
+          white_belt_form: [1, 2],
+          combination: [1, 2, 3, 4, 5],
+          step_match: [1, 2],
+          kick: ["front kick", "turn kick"],
+          sparring: "1:1",
+          breaking: ["palm", "front kick"],
+        },
+        yellow: {
+          palgwae: [],
+          taeguek: [],
+          white_belt_form: [1, 2],
+          combination: [1, 2, 3, 4, 5],
+          step_match: [1, 2],
+          kick: ["front kick", "turn kick"],
+          sparring: "1:1",
+          breaking: ["palm", "front kick"],
+        },
+        green: {
+          palgwae: [],
+          taeguek: [],
+          white_belt_form: [1, 2],
+          combination: [1, 2, 3, 4, 5],
+          step_match: [1, 2],
+          kick: ["front kick", "turn kick"],
+          sparring: "1:1",
+          breaking: ["palm", "front kick"],
+        },
+        blue: {
+          palgwae: [],
+          taeguek: [],
+          white_belt_form: [1, 2],
+          combination: [1, 2, 3, 4, 5],
+          step_match: [1, 2],
+          kick: ["front kick", "turn kick"],
+          sparring: "1:1",
+          breaking: ["palm", "front kick"],
+        },
+        red: {
+          palgwae: [],
+          taeguek: [],
+          white_belt_form: [1, 2],
+          combination: [1, 2, 3, 4, 5],
+          step_match: [1, 2],
+          kick: ["front kick", "turn kick"],
+          sparring: "1:1",
+          breaking: ["palm", "front kick"],
+        },
+        first_dan_evaluation: {
+          palgwae: [],
+          taeguek: [],
+          white_belt_form: [1, 2],
+          combination: [1, 2, 3, 4, 5],
+          step_match: [1, 2],
+          kick: ["front kick", "turn kick"],
+          sparring: "1:1",
+          breaking: ["palm", "front kick"],
+        },
+      },
+    },
+  },
+];
+
+const belt_colors = [
+  {
+    id: 1,
+    displayColor: "White",
+    key: "white",
+  },
+  {
+    id: 2,
+    displayColor: "White Yellow Stripe",
+    key: "white_yellow_stripe",
+  },
+  {
+    id: 3,
+    displayColor: "Yellow",
+    key: "yellow",
+  },
+  {
+    id: 4,
+    displayColor: "Yellow Green Stripe",
+    key: "yellow_green_stripe",
+  },
+  {
+    id: 5,
+    displayColor: "Green",
+    key: "green",
+  },
+  {
+    id: 6,
+    displayColor: "Green Blue Stripe",
+    key: "green_blue_stripe",
+  },
+  {
+    id: 7,
+    displayColor: "Red",
+    key: "red",
+  },
+];
+
+module.exports = { menu, belt_test_requirements, belt_colors };
